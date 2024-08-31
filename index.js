b54e8fb8962b926b192f55a591207e9efbb99373
luke-taful <luke.taful@gmail.com>
2024-08-31 13:29:25 +0100

    Fixed background effects, added more content

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Panel Cloning
 const container = document.querySelector(".container"),  
     panel = document.querySelector(".panel");
     sheet = document.querySelector("#sheet");
@@ -9,3 +10,42 @@ for(let i = 0; i < 24; i++) {
 for(let i = 0; i < 377; i++) {
     sheet.appendChild(container.cloneNode(true));
 }
+
+//Background Panel Effects
+document.onmousemove = e => {
+  var cursorX = e.clientX;
+  var cursorY = e.clientY;
+  let elements = document.elementsFromPoint(cursorX, cursorY);
+    elements.forEach((item) => {
+
+      if(item.className == "panel"){
+        let close = item.closest(".container");
+        let prev = item.previousElementSibling
+        let nxt = item.nextElementSibling
+
+        item.style.background = `#bad4f35f`;
+
+        if(prev != null){
+          prev.style.background = `#bad4f35f`;
+          setTimeout(() => { prev.style.background = `#00000000`; }, 300);
+        }
+        if(nxt != null){
+          nxt.style.background = `#bad4f35f`;
+          setTimeout(() => { nxt.style.background = `#00000000`; }, 300);
+        }
+        close.style.background = `#5ba5ff5f`;
+        setTimeout(() => { item.style.background = `#00000000`; }, 500);
+        setTimeout(() => { close.style.background = `#00000000`; }, 500);
+      }
+    });
+  };
+
+  
+// function panelLight(e){
+//   let item = e.target;
+//         item.style.background = `#bad4f35f`;
+// };
+// function panelLightOff(e){
+//   let item = e.target;
+//   setTimeout(() => {  item.style.background = `#00000000`; }, 500);
+// };
